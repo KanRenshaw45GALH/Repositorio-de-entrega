d0e017270756fc8c9b66df99186024c5168f23d6
KanRenshaw45GALH <163915505+KanRenshaw45GALH@users.noreply.github.com>
2024-05-06 12:33:01 -0600

    Ejercicio de While

---
+++ While.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+    {
+        int i=1;
+        int Resultado=0;
+        while (i<10)
+       {
+            cout << i <<¨\n¨;
+            i++;
+        //}
+        //while(i<13)
+            //{
+                //Resultado= 12 * i;
+                //cout << "12 * "<< i << "=" << Resultado << endl;
+                //i++;
+        }
+        
+        return 0;
+    }
